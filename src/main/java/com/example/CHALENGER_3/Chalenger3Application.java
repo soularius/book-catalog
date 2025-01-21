5a7b2a8dd2392a23701b0d45f5ce487a4d11c48f
Yined Molina <yined.molina@gmail.com>
2025-01-21 13:25:45 -0500

    first commit

---
+++ src/main/java/com/example/CHALENGER_3/Chalenger3Application.java
@@ -0,0 +1,13 @@
+package com.example.CHALENGER_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chalenger3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chalenger3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CHALENGER_3/Chalenger3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CHALENGER_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chalenger3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
